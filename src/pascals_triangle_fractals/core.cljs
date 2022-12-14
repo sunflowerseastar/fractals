8f001df4e7853d19763a4d2529b61260d45667bf
Grant Surlyn <grant@sunflowerseastar.com>
2022-12-14 15:54:22 -0500

    add basic 'num-rows' and 'num-cells' meta

--- src/pascals_triangle_fractals/core.cljs
+++ src/pascals_triangle_fractals/core.cljs
@@ -4,30 +4,7 @@
    [reagent.core :as reagent :refer [atom]]
    [reagent.dom :as rdom]))
 
-;; [[1 0 0 0 0 0 0]
-;;  [1 1 0 0 0 0 0]
-;;  [1 1 1 0 0 0 0]
-;;  [1 1 1 1 0 0 0]
-;;  [1 1 1 1 1 0 0]
-;;  [1 1 1 1 1 1 0]
-;;  [1 1 1 1 1 1 1]]
-
-;; [[1]
-;;  [1 1]
-;;  [1 1 1]
-;;  [1 1 1 1]
-;;  [1 1 1 1 1]
-;;  [1 1 1 1 1 1]
-;;  [1 1 1 1 1 1 1]]
-
-;; [[1]
-;;  [1 1]
-;;  [1 2 1]
-;;  [1 3 3 1]
-;;  [1 4 6 4 1]
-;;  [1 5 10 10 5 1]
-;;  [1 6 15 20 15 6 1]]
-
+;; Pascal (and sierpinski) generation
 
 (defn row->next-row
   "Given a yield fn and a row, generate the next row."
@@ -74,69 +51,71 @@
 (def generate-sierpinski (partial pascal-rows sierpinski-yield))
 (def generate-pascal (partial pascal-rows pascal-yield))
 
-(generate-sierpinski 10)
-(generate-pascal 10)
+;; (generate-sierpinski 10)
+;; (generate-pascal 10)
+
+
+
+;; utility
+
+(def scale 1.6)
+
+(defn inner-width->num-rows [inner-width]
+  (js/parseInt (/ (* .95 inner-width) scale)))
+
+;; state
+
+(def window-width (atom nil))
+(def num-rows (atom (inner-width->num-rows (.-innerWidth js/window))))
+(def num-cells (atom 443))
 
 
 ;; canvas
-;;
-(defn clear!
-  [canvas]
-  (let [context (.getContext canvas "2d")]
-    (.scale context 1 1)
-    (.clearRect context 0 0 (.-width canvas) (.-height canvas))))
 
-;; (defn draw-x [size]
-;;   (let [img (BufferedImage. size size BufferedImage/TYPE_INT_ARGB)
-;;         plot-rows (generate-sierpinski size)
-;;         plots (for [x (range 0 size)
-;;                     y (range 0 x)]
-;;                 (if (= 1 (get (get plot-rows x) y)) [x y]))
-;;         gfx (.getGraphics img)]
-;;     (.setColor gfx Color/WHITE)
-;;     (.fillRect gfx 0 0 size size)
-;;     (.setColor gfx Color/BLACK)
-;;     (doseq [p (filter (comp not nil?) plots)]
-;;       (.drawLine gfx
-;;                  (get p 0)
-;;                  (get p 1)
-;;                  (get p 0)
-;;                  (get p 1)))
-;;     (ImageIO/write img "png" (File. "/result-asdf.png"))))
+;; (generate-sierpinski 5) => ...
+;; [[1]
+;;  [1 1]
+;;  [1 0 1]
+;;  [1 1 1 1]
+;;  [1 0 0 0 1]
+;;  [. . . . . .]]
 
+;; Imagining each 1 as a black square, the above will be drawn as:
+
+;; 1 1 1 1 1 ...
+;;   1 0 1 0 ...
+;;     1 1 0 ...
+;;       1 0 ...
+;;         1 ...
+;;           ...
+
+;; It's as if the x and y were flipped.
 
 (defn draw!
   [canvas size]
+  (println "draw!: " size)
   (let [context (.getContext canvas "2d")
         sierpinski-rows (generate-sierpinski size)
+        new-num-cells (reduce + (range 1 (inc size)))
         ;; each 1 in the sierpinski-rows is a "plot"
         plots (for [x (range 0 size)
                     y (range 0 (inc x))
                     :when (= 1 (get (get sierpinski-rows x) y))]
                 [x y])]
-    ;; (println size)
-    ;; (println sierpinski-rows)
-    ;; (println plots)
-    ;; (println px)
-    (.scale context 10 10)
+    (reset! num-cells new-num-cells)
+    (.scale context scale scale)
     (doseq [p plots]
       (.fillRect context (get p 0) (get p 1)
                  1 1))))
 
-;; canvas control and general DOM
-
-(def window-width (reagent/atom nil))
-
 (defn render-canvas!
-  "Initiate and control a canvas rendering."
-  []
+  [num-rows-atom]
   (let [dom-node (reagent/atom nil)]
     (reagent/create-class
      {:component-did-update
       (fn [this]
         (let [canvas (.-firstChild @dom-node)]
-          ;; (clear! canvas)
-          (draw! canvas 60)))
+          (draw! canvas @num-rows-atom)))
 
       :component-did-mount
       (fn [this]
@@ -144,35 +123,31 @@
 
       :reagent-render
       (fn []
-        @window-width ;; Trigger re-render on window resizes
+        @window-width ;; trigger re-render
         [:div.canvas-container
-         ;; reagent-render is called before the compoment mounts, so protect
-         ;; against the null dom-node that occurs on the first render
          [:canvas (if-let [node @dom-node]
                     {:width (.-clientWidth node) :height (.-clientHeight node)})]])})))
 
-
 (defn main []
   [:<>
-   [:div.pre-canvas [:p "pre"]]
-   [:div.canvas-outer-container [render-canvas!]]
-   [:div.post-canvas [:p "post"]]])
+   [:div.pre-canvas
+    [:p [:span "num-rows: " @num-rows " | cells: " @num-cells]]]
+   [render-canvas! num-rows]
+   [:p ""]])
 
 
 ;; reagent setup + resizing
 
 (defn on-window-resize [evt]
-  (reset! window-width (.-innerWidth js/window)))
+  (reset! window-width (.-innerWidth js/window))
+  (reset! num-rows (inner-width->num-rows (.-innerWidth js/window))))
 
 (defn mount [el]
   (rdom/render [main] el)
   (.addEventListener js/window "resize" on-window-resize))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
-
 (defn mount-app-element []
-  (when-let [el (get-app-element)]
+  (when-let [el (gdom/getElement "app")]
     (mount el)))
 
 ;; conditionally start your application based on the presence of an "app" element
