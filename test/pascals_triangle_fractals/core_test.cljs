05fd85cc109a4338c070ba75d576936def805c9d
Grant Surlyn <grant@sunflowerseastar.com>
2022-12-14 10:48:27 -0500

    init

---
+++ test/pascals_triangle_fractals/core_test.cljs
@@ -0,0 +1,10 @@
+(ns pascals-triangle-fractals.core-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [pascals-triangle-fractals.core :refer [multiply]]))
+
+(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
