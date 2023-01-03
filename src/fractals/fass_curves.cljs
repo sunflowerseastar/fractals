7cb17cbac370ff944c73827950ae8c1daeb1992b
Grant Surlyn <grant@sunflowerseastar.com>
2023-01-03 12:18:07 -0500

    add Moore curve; re-organize fass curves

--- src/fractals/fass_curves.cljs
+++ src/fractals/fass_curves.cljs
@@ -3,8 +3,7 @@
    [fractals.l-system :refer [l-system]]))
 
 (def fass-variations
-  [
-   {:name "quad gosper"
+  [{:name "quad Gosper"
     :variables #{:X :Y}
     :constants #{:+ :-}
     :start [:- :Y]
@@ -18,7 +17,7 @@
     :delta 90
     :initial-num-iterations 2
     :max-iterations 3}
-   {:name "hex gosper"
+   {:name "hex Gosper"
     :variables #{:X :Y}
     :constants #{:+ :-}
     :start [:X]
@@ -27,7 +26,27 @@
     :actions {:X :forward :Y :forward :+ :left :- :right}
     :delta 60
     :initial-num-iterations 3
-    :max-iterations 6}])
+    :max-iterations 6}
+   {:name "7x7"
+    :variables #{:X :Z}
+    :constants #{:+ :-}
+    :start [:X]
+    :rules {:X [:X :X :X :- :Z :Z :- :Z :- :X :+ :Z :+ :X :X :+ :Z :- :X :Z :- :Z :- :X :+ :X :+ :Z :Z :+ :X :X :X :+ :Z :- :X :Z :- :Z :- :X :+ :X :+ :Z :Z :+ :X :X :X :+ :Z :X :Z :- :Z :- :X :+ :Z :X :- :Z :Z :- :X :- :Z :+ :X :+ :X :X :+ :Z :Z :Z :-]
+            :Z [:+ :X :X :X :- :Z :Z :- :Z :- :X :+ :Z :+ :X :X :+ :Z :X :- :Z :+ :X :+ :X :Z :X :- :Z :Z :Z :- :X :X :- :Z :- :Z :+ :X :+ :X :Z :+ :X :- :Z :Z :Z :- :X :X :- :Z :- :Z :+ :X :+ :X :Z :+ :X :- :Z :Z :- :X :- :Z :+ :X :+ :X :X :+ :Z :Z :Z]}
+    :actions {:X :forward :Z :forward :+ :left :- :right}
+    :delta 90
+    :initial-num-iterations 2
+    :max-iterations 3}
+   {:name "9x9"
+    :variables #{:L :R}
+    :constants #{:+ :-}
+    :start [:L]
+    :rules {:L [:R :R :- :L :- :L :+ :R :+ :R :L :+ :R :- :L :R :+ :L :- :R :- :L :+ :R :L :L :- :R :- :L :R :L :R :L :+ :R :- :L :R :L :R :+ :L :+ :R :R :L :R :L :+ :R :- :L :R :L :- :L :- :R :L :+ :R :- :L :R :L :R :L :+ :R :+ :R :L :R :L :R :L :+ :R :- :L :L :- :R :- :L :+ :R :L :L :- :R :- :L :R :+ :R :+ :L :- :L :- :R :+ :R :+ :L :L :- :R :- :L :R :+ :R :+ :L :- :L :- :R :+ :R :+ :L :L :+]
+            :R [:- :R :R :- :L :- :L :+ :R :+ :R :- :L :- :L :R :+ :L :+ :R :R :- :L :- :L :+ :R :+ :R :- :L :- :L :R :+ :L :+ :R :R :L :- :R :+ :L :+ :R :R :+ :L :- :R :L :R :L :R :L :- :L :- :R :L :R :L :R :+ :L :- :R :L :+ :R :+ :R :L :R :+ :L :- :R :L :R :L :L :- :R :- :L :R :L :R :+ :L :- :R :L :R :L :R :+ :L :+ :R :R :L :- :R :+ :L :+ :R :- :L :R :+ :L :- :R :L :- :L :- :R :+ :R :+ :L :L]}
+    :actions {:L :forward :R :forward :+ :left :- :right}
+    :delta 90
+    :initial-num-iterations 1
+    :max-iterations 3}])
 
 (defn fass-curves [window-width]
   (l-system window-width fass-variations))
--- src/fractals/more_fass_curves.cljs
+++ src/fractals/more_fass_curves.cljs
@@ -3,17 +3,7 @@
    [fractals.l-system :refer [l-system]]))
 
 (def fass-variations
-  [{:name "Hilbert"
-    :variables #{:L :R}
-    :constants #{:F :+ :-}
-    :start [:L]
-    :rules {:L [:+ :R :F :- :L :F :L :- :F :R :+]
-            :R [:- :L :F :+ :R :F :R :+ :F :L :-]}
-    :actions {:F :forward :+ :left :- :right}
-    :delta 90
-    :initial-num-iterations 4
-    :max-iterations 8}
-   {:name "Peano"
+  [{:name "Peano"
     :variables #{:L :R}
     :constants #{:F :+ :-}
     :start [:L]
@@ -27,6 +17,24 @@
     :delta 45
     :initial-num-iterations 2
     :max-iterations 3}
+   {:name "Hilbert"
+    :variables #{:L :R}
+    :constants #{:F :+ :-}
+    :start [:L]
+    :rules {:L [:+ :R :F :- :L :F :L :- :F :R :+]
+            :R [:- :L :F :+ :R :F :R :+ :F :L :-]}
+    :actions {:F :forward :+ :left :- :right}
+    :delta 90
+    :initial-num-iterations 4
+    :max-iterations 8}
+   {:name "Moore"
+    :variables #{:L :R}
+    :start [:L :F :L :+ :F :+ :L :F :L]
+    :rules {:L [:- :R :F :+ :L :F :L :+ :F :R :-]
+            :R [:+ :L :F :- :R :F :R :- :F :L :+]}
+    :actions {:F :forward :+ :left :- :right}
+    :delta 90
+    :max-iterations 7}
    {:name "3x3"
     :variables #{:L :R}
     :start [:- :L]
@@ -46,27 +54,7 @@
     :actions {:F :forward :+ :left :- :right}
     :delta 90
     :initial-num-iterations 2
-    :max-iterations 4}
-   {:name "7x7"
-    :variables #{:X :Z}
-    :constants #{:+ :-}
-    :start [:X]
-    :rules {:X [:X :X :X :- :Z :Z :- :Z :- :X :+ :Z :+ :X :X :+ :Z :- :X :Z :- :Z :- :X :+ :X :+ :Z :Z :+ :X :X :X :+ :Z :- :X :Z :- :Z :- :X :+ :X :+ :Z :Z :+ :X :X :X :+ :Z :X :Z :- :Z :- :X :+ :Z :X :- :Z :Z :- :X :- :Z :+ :X :+ :X :X :+ :Z :Z :Z :-]
-            :Z [:+ :X :X :X :- :Z :Z :- :Z :- :X :+ :Z :+ :X :X :+ :Z :X :- :Z :+ :X :+ :X :Z :X :- :Z :Z :Z :- :X :X :- :Z :- :Z :+ :X :+ :X :Z :+ :X :- :Z :Z :Z :- :X :X :- :Z :- :Z :+ :X :+ :X :Z :+ :X :- :Z :Z :- :X :- :Z :+ :X :+ :X :X :+ :Z :Z :Z]}
-    :actions {:X :forward :Z :forward :+ :left :- :right}
-    :delta 90
-    :initial-num-iterations 2
-    :max-iterations 3}
-   {:name "9x9"
-    :variables #{:L :R}
-    :constants #{:+ :-}
-    :start [:L]
-    :rules {:L [:R :R :- :L :- :L :+ :R :+ :R :L :+ :R :- :L :R :+ :L :- :R :- :L :+ :R :L :L :- :R :- :L :R :L :R :L :+ :R :- :L :R :L :R :+ :L :+ :R :R :L :R :L :+ :R :- :L :R :L :- :L :- :R :L :+ :R :- :L :R :L :R :L :+ :R :+ :R :L :R :L :R :L :+ :R :- :L :L :- :R :- :L :+ :R :L :L :- :R :- :L :R :+ :R :+ :L :- :L :- :R :+ :R :+ :L :L :- :R :- :L :R :+ :R :+ :L :- :L :- :R :+ :R :+ :L :L :+]
-            :R [:- :R :R :- :L :- :L :+ :R :+ :R :- :L :- :L :R :+ :L :+ :R :R :- :L :- :L :+ :R :+ :R :- :L :- :L :R :+ :L :+ :R :R :L :- :R :+ :L :+ :R :R :+ :L :- :R :L :R :L :R :L :- :L :- :R :L :R :L :R :+ :L :- :R :L :+ :R :+ :R :L :R :+ :L :- :R :L :R :L :L :- :R :- :L :R :L :R :+ :L :- :R :L :R :L :R :+ :L :+ :R :R :L :- :R :+ :L :+ :R :- :L :R :+ :L :- :R :L :- :L :- :R :+ :R :+ :L :L]}
-    :actions {:L :forward :R :forward :+ :left :- :right}
-    :delta 90
-    :initial-num-iterations 1
-    :max-iterations 3}])
+    :max-iterations 4}])
 
 (defn more-fass-curves [window-width]
   (l-system window-width fass-variations))
